dad893a3cdbde1bbcf774cdbb8530528a6ffeeac
isayme <isaymeorg@gmail.com>
2016-01-19 22:11:49 +0800

    update tests

--- test/index.js
+++ test/index.js
@@ -3,10 +3,6 @@ var processon = require('../index');
 
 var access_token = process.env.PROCESSON_ACCESS_TOKEN;
 
-var client = new processon.Client({
-  access_token: access_token
-});
-
 describe('Client constructor', function() {
   it('should ok if access_token from string', function() {
     var c = new processon.Client('access_token');
@@ -34,16 +30,22 @@ describe('Client constructor', function() {
 });
 
 describe('Client APIs', function() {
+  var client = null;
+
   this.timeout(0);
 
   it('process.env.PROCESSON_ACCESS_TOKEN should set for test', function() {
     expect(access_token).to.be.a('string');
+
+    client = new processon.Client({
+      access_token: access_token
+    });
   });
 
   it('user info', function(done) {
     client.getUser(function(err, result) {
       expect(err).to.be.null;
-      expect(result.status.toLowerCase()).to.be.equal('success');
+      expect(result.status).to.be.equal('success');
       expect(result.data).to.include.keys(['_id', 'userName', 'email', 'profileUrl']);
       done();
     });
@@ -52,7 +54,7 @@ describe('Client APIs', function() {
   it('get user diagrams', function(done) {
     client.getDiagrams(function(err, result) {
       expect(err).to.be.null;
-      expect(result.status.toLowerCase()).to.be.equal('success');
+      expect(result.status).to.be.equal('success');
       expect(result.data).to.be.a('array');
       done();
     });
@@ -61,7 +63,7 @@ describe('Client APIs', function() {
   it('get user recent diagrams', function(done) {
     client.getRecentDiagrams(function(err, result) {
       expect(err).to.be.null;
-      expect(result.status.toLowerCase()).to.be.equal('success');
+      expect(result.status).to.be.equal('success');
       expect(result.data).to.be.a('array');
       done();
     });
@@ -70,7 +72,7 @@ describe('Client APIs', function() {
   it('get user clloa diagrams', function(done) {
     client.getCollaDiagrams(function(err, result) {
       expect(err).to.be.null;
-      expect(result.status.toLowerCase()).to.be.equal('success');
+      expect(result.status).to.be.equal('success');
       // expect(result.data).to.be.a('array');
       done();
     });
